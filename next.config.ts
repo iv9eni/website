dba7c7ac31546c5b326ba2b6a5a3a32ad01e594b
Ivgeni Darinski <3kbjutpr@anonaddy.me>
2025-05-02 22:59:15 -0400

    Add GitHub pages support and Actions setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static export
+
+  // Set base path for GitHub pages
+  basePath: process.env.GITHUB_PAGES ? "/website" : "",
+
+  // Configures asset prefix for GitHub pages
+  assetPrefix: process.env.GITHUB_PAGES
+    ? "https://iv9eni.github.io/website/"
+    : undefined,
+
+  // Disable server-side rendering since GitHub pages doesn't support it
+  trailingSlash: true,
+
+  // Configure images for static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
